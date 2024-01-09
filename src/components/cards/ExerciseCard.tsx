fd3f28100d711c4c0600b77eb96c2e0ee76e0fc4
tomimarkus991 <tomimarkusalber@gmail.com>
2024-01-09 15:18:41 +0200

    Refactor ExerciseCard component

--- src/components/cards/ExerciseCard.tsx
+++ src/components/cards/ExerciseCard.tsx
@@ -1,3 +1,5 @@
+import { secondsToMinutes } from "date-fns";
+
 interface Props {
   sets: number;
   reps: number;
@@ -8,14 +10,14 @@ interface Props {
 
 export const ExerciseCard = ({ duration, reps, rest, sets, name }: Props) => {
   return (
-    <div className="p-2 border border-blue-600 rounded-lg">
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-row">
+    <div className="p-2 border border-blue-600 rounded-lg whitespace-nowrap">
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row items-center">
           <p>{sets}x</p>
           <p>{reps === 0 && duration > 0 ? `${duration}s` : `${reps} reps`}</p>
-          <p className="ml-1">{name}</p>
+          <p className="ml-3 mr-2 whitespace-normal">{name}</p>
         </div>
-        {rest < 60 ? <p>{rest} sec rest</p> : <p>{(Number(rest) / 60).toFixed(1)} min rest</p>}
+        {rest < 60 ? <p>{rest} sec rest</p> : <p>{secondsToMinutes(rest)} min rest</p>}
       </div>
     </div>
   );
