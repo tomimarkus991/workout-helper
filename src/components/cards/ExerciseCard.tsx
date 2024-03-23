e36ad474467d81e61f5f0e2514db3460d065c3e6
tomimarkus991 <tomimarkusalber@gmail.com>
2024-03-23 17:43:32 +0200

    Update reps display in ExerciseCard component

--- src/components/cards/ExerciseCard.tsx
+++ src/components/cards/ExerciseCard.tsx
@@ -14,7 +14,9 @@ export const ExerciseCard = ({ duration, reps, rest, sets, name }: Props) => {
       <div className="flex flex-row items-center justify-between">
         <div className="flex flex-row items-center">
           <p>{sets}x</p>
-          <p>{reps === 0 && duration > 0 ? `${duration}s` : `${reps} reps`}</p>
+          <p>
+            {reps === 0 && duration > 0 ? `${duration}s` : `${reps === 999 ? "Max" : reps} reps`}
+          </p>
           <p className="ml-3 mr-2 whitespace-normal">{name}</p>
         </div>
         {rest < 60 ? <p>{rest}s rest</p> : <p>{secondsToMinutes(rest)}m rest</p>}
